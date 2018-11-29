98d1ca27341af4069d80f922add693a99c4cb985
Andrei Lukashevich <Andrei_Lukashevich1@epam.com>
2018-11-29 14:40:59 +0300

    settle studentsa in social behavior

--- Project/Service/Hostel.cs
+++ Project/Service/Hostel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data;
+using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +10,27 @@ namespace Service
 {
     class Hostel
     {
-        private int _numberOfSeats;
-        public int GetNumberOfSeatsForGroup(string facultu, int group)
+        private int _numberOfSeats=100;
+     
+        DataBase dataBase = new DataBase();
+        public void SettleStudent()
         {
-            return 1;
+            int temp = _numberOfSeats;
+            List<Student> students = dataBase.Students;
+            var priviligiesStudents = students.Where(n => n.Privileges == true);
+            foreach (Student student in priviligiesStudents)
+            {
+                student.LiveInSocialBehavior = true;
+            }
+            temp -= priviligiesStudents.Count();
+            var otherStudents = students.Except(priviligiesStudents).OrderBy(n => n.AverageBall);
+            foreach(Student student in otherStudents)
+            {
+                if (temp != 0)
+                    student.LiveInSocialBehavior = true;
+                else
+                    break;
+            }
         }
     }
 }
--- Project/entities/Persons/Student.cs
+++ Project/entities/Persons/Student.cs
@@ -13,17 +13,92 @@ namespace Entities
         private bool _liveInSocialBehavior;
         private string _specialty;
         private int _course;
-        private bool _inInBudget;
+        private bool _inBudget;
         private int _group;
         private double _averageBall;
         private double _scholarship;
         private List<int> marks;
 
-        //public Student(string Name,string Password)
-        //{
-        //    Data.GetStudentWithLogin(Name);
-        //}
-        public int Group { get; set; }
+        public bool LiveInSocialBehavior
+        {
+            get
+            {
+                return _liveInSocialBehavior;
+            }
+            set
+            {
+                _liveInSocialBehavior = value;
+            }
+        }
+        public bool Privileges
+        {
+            get
+            {
+                return _privileges;
+            }
+            set
+            {
+                _privileges = value;
+            }
+        }
+        public double Scholarship
+        {
+            get
+            {
+                return _scholarship;
+            }
+            set
+            {
+                _scholarship = value;
+            }
+        }
+        public bool InBudget
+        {
+            get
+            {
+                return _inBudget;
+            }
+            set
+            {
+                _inBudget = value;
+            }
+        }
+
+        public int Group
+        {
+            get
+            {
+                return _group;
+            }
+
+            set
+            {
+                _group = value;
+            }
+        }
+        public string Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status=value;
+            }
+        }
+
+        public double AverageBall
+        {
+            get
+            {
+                return _averageBall;
+            }
+            set
+            {
+                _averageBall = value;
+            }
+        }
 
         public void SetMark(int mark)
         {
