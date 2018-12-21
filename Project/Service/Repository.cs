003d980579e0f30126bfdeef3343936ed5780c5f
Andrysha <mnepoh1998@gmail.com>
2018-12-22 02:27:03 +0300

    create instance of repo

--- Project/Service/Repository.cs
+++ Project/Service/Repository.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Service
 {
-    class Repository
+    internal class Repository
     {
+        public List<Admin> _admins;
+        public List<Discipline> _disciplines;
+        public List<Students> _students;
+        public List<Teacher> _teachers;
+        public Repository()
+        {
+            Lists BD = new Lists();
+            _admins = BD.admins;
+            _disciplines = BD.disciplines;
+            _students = BD.students;
+            _teachers = BD.teachers;
+        }
+
     }
 }
